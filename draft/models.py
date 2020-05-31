1df9693ff066decc74cf772a2e7e9ff1c74123f0
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-31 14:02:57 -0500

    Initial commit

---
+++ draft/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+#League
+class League(models.Model):
+    leagueId = models.IntegerField()
+    teams = models.IntegerField(default=10)
+    rounds = models.IntegerField(default=4)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    draft_order = models.CharField(max_length=500)
+    curr_round = models.IntegerField()
+    curr_pick = models.IntegerField()
+    date_created = models.DateTimeField(default=timezone.now)
+    unique_key = models.CharField(max_length=50, unique=True) #leagueId+username
+    year = models.IntegerField()
+
+    def __str__(self):
+        return 'League ' + str(self.leagueId)
+
+    def get_year(self):
+        if self.date_created.month < 5:
+            return self.date_created.year-1
+        return self.date_created.year
+
+
+#Free Agents in a league        
+class Player(models.Model):
+    rank = models.IntegerField()
+    name = models.CharField(max_length=50)
+    team = models.CharField(max_length=5)
+    position = models.CharField(max_length=5)
+    projection = models.IntegerField()
+    points = models.IntegerField()
+    drafted = models.BooleanField()
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+#Picks in a league
+class Pick(models.Model):
+    round = models.IntegerField()
+    number = models.IntegerField()
+    player = models.ForeignKey(Player, on_delete=models.DO_NOTHING)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Pick ' + str(self.round) + '.' + str(self.number)
+
+#Teams in a league
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    def __str(self):
+        return self.name
+        
+
+
---
+++ draft/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from .views import LeagueListView
+
+urlpatterns = [
+    path('', views.home, name='draft-home'),
+    path('draft/', views.draft, name='draft-room'),
+    path('reset/<int:id>/', views.reset, name='create-draft'),
+    path('draft/<int:id>/', views.access, name='draft-room'),
+    path('league/', views.find, name='searched-league'),
+    path('league-list/<int:id>/', LeagueListView.as_view(), name='league-list'),
+    path('save/<int:id>/', views.saveorder, name='save-draft-order'),
+    path('view-only/<str:key>/', views.viewonly, name='view-only'),
+    path('draft/<int:id>/<int:rank>/', views.pickplayer, name='pick-player'),
+    path('undo/<int:id>/', views.undo, name='undo-pick'),
+    path('trade/<int:id>/', views.trade, name='trade-pick'),
+    path('delete/<int:id>/', views.delete, name='delete-league'),
+    path('delete/<int:id>/confirm/', views.delete_confirm, name='delete-confirm'),
+]
